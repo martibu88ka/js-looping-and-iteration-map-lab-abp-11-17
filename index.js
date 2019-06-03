343d8344e17b7bc9f5a4370157d1fb69b0d6e29e
Martyna <martyna.glaz@gmail.com>
2019-06-03 00:32:52 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // Code your solution in this file.
+function lowerCaseDrivers(array){
+return array.map(function(e){return e.toLowerCase()} )
+}
+function nameToAttributes(names){
+  return names.map(function(name){
+  let fName= name.split(" ")[0]
+  let lName= name.split(" ")[1]
+  return ({firstName:fName, lastName:lName})
+})
+}
+function attributesToPhrase(homie){
+  return homie.map(function(name)){
+    let fName= name.split (" ")[0]
+    let fHome=name.split (" ")[1]
+    console.log (${fName} is from ${fHome})
+  }
+
+}
